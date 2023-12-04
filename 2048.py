2aadda88b5ac1d03738cd159a44b0d4f4e4e4528
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2023-12-04 14:31:44 +0100

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,5 +1,6 @@
 import random
 import os
+from Tools import *
 
 SIZE:int=4
 grid:list=[]
@@ -10,12 +11,12 @@ def generateGrid()->None:
         grid.append([])
         
         for j in range(SIZE):
-            grid[i].append("")
+            grid[i].append("□")
 
 def checkEmptySlots()->None:
     for i in range(SIZE):
         for j in range(SIZE):
-            if grid[i][j]=="":
+            if grid[i][j]=="□":
                 emptySlots.append((i,j))
             
 def getRandomPos()->int:
@@ -27,20 +28,31 @@ def randomGen():
     randomPos:int = getRandomPos()
     
     if random.randint(1,10)<9:
-        grid[emptySlots[randomPos][0]][emptySlots[randomPos][1]]=2
+        grid[emptySlots[randomPos][0]][emptySlots[randomPos][1]]="2"
     else:
-        grid[emptySlots[randomPos][0]][emptySlots[randomPos][1]]=4
+        grid[emptySlots[randomPos][0]][emptySlots[randomPos][1]]="4"
 
 def init():
     generateGrid()
     
     for i in range(2):
         randomGen()
+        
+def move(direction=str): 
+    if direction=="up":
+        pass
     
+def printGrid():
+    for i in grid:
+        print(" ".join(i))
+              
 def jeu():
     
     init()
-    while True:
-        
+    printGrid()
+    """while True:
+        playMove:str= AskInputs("Choisissez une direction (z,q,s,d):", ["z","q","s","d"])
+        if playMove=="z":
+            move("up")"""
 
 jeu()
